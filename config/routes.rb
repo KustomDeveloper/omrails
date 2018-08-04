93ea4a37e1773b9531f7d0be37092222cad37a26
Kustomdesigner <kustomdesigner@gmail.com>
2018-08-03 22:21:44 -0400

    fix a few bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
+  devise_for :users
+  as :user do
+    get 'signin', to: 'devise/sessions#new'
+    delete 'signout', to: 'devise/sessions#destroy'
+    get "signup", to: 'devise/registrations#new'
+  end
 
-  get 'feed', to: 'feed#show'
+  
   
   resources :users, only: :show, param: :username do
     member do 
@@ -11,19 +18,11 @@ Rails.application.routes.draw do
   
   resources :items
   resources :tweets
-  ActiveAdmin.routes(self)
-  devise_for :users
-
-  as :user do
-  	get 'signin', to: 'devise/sessions#new'
-  	delete 'signout', to: 'devise/sessions#destroy'
-  	get "signup", to: 'devise/registrations#new'
-  end
 
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact-us', to: 'pages#contact-us'
-
+  get 'feed', to: 'feed#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
